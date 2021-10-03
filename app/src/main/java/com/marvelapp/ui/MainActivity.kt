c7933b32cc9cdf6ac9684f62e4a6f54dbe9fc38c
Sumit Singh <sumit.singh@agribazaar.com>
2021-10-03 12:50:37 +0530

    Setup fragment nav controler

--- app/src/main/java/com/marvelapp/ui/MainActivity.kt
+++ app/src/main/java/com/marvelapp/ui/MainActivity.kt
@@ -2,11 +2,30 @@ package com.marvelapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.marvelapp.R
+import com.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
     }
+
+    private fun setupNavigation(){
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.character->findNavController(binding.frameContainerView.id).navigate(R.id.frg_character)
+                R.id.comic->findNavController(binding.frameContainerView.id).navigate(R.id.frg_comic)
+            }
+            true
+        }
+    }
+
 }

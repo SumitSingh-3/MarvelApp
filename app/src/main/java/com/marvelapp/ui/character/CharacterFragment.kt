d58c26fe8845f818a3b06b54433da8d66f925aa4
Sumit Singh <sumit.singh@agribazaar.com>
2021-10-06 16:33:30 +0530

    Create single viewmodal so when change in fragment not reload fragment

--- app/src/main/java/com/marvelapp/ui/character/CharacterFragment.kt
+++ app/src/main/java/com/marvelapp/ui/character/CharacterFragment.kt
@@ -6,20 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelLazy
 import androidx.recyclerview.widget.GridLayoutManager
-import com.marvelapp.R
 import com.marvelapp.databinding.FragmentCharacterBinding
 import com.marvelapp.db.SearchHistory
 import com.marvelapp.db.SearchHistoryRepository
 import com.marvelapp.modals.base.PagingListResponse
 import com.marvelapp.ui.character.paging.CharacterPagingAdapter
 import com.marvelapp.utils.AppUtil
-import com.marvelapp.viewmodal.CharacterViewModal
 import com.marvelapp.viewmodal.MarvelViewModal
 
 
--- app/src/main/java/com/marvelapp/ui/comic/ComicFragment.kt
+++ app/src/main/java/com/marvelapp/ui/comic/ComicFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelLazy
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.chip.Chip
